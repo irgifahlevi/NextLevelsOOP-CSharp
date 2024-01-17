068fc3584a2dc6e6069ee7887c2444bbe0dd6ff4
irgifahlevi <irgifahlevi5@gmail.com>
2024-01-17 13:50:48 +0700

    property & function abstract

---
+++ Chapter4/AbstractPropsFunc/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+// instance the class Car & Motorcycle
+using AbstractPropsFunc;
+Car cars = new Car() { Make = "Ferari", Model = "Escort" };
+Motorcycle motor = new Motorcycle() { Make = "Trumph", Model = "Thunder" };
+
+Console.WriteLine($"{cars}");
+cars.SoundHorn();
+Console.WriteLine($"{motor}");
+motor.SoundHorn();
+
+// abstract class tidak bisa di instance
+// Vehicle vehicle = new Vehicle();
---
+++ Chapter4/AbstractPropsFunc/Vehicle.cs
@@ -0,0 +1,94 @@
+namespace AbstractPropsFunc
+{
+    public abstract class Vehicle
+    {
+        /// <summary>
+        /// Kelas abstrac kelas yang tidak dapat diinstansiasi (dibuat objeknya) secara langsung
+        /// kelas abstract digunakan sebagai kerangka dasar untuk kelas-kelas turunannya.
+        /// </summary>
+        /// <returns></returns>
+        #region Constructor
+        public Vehicle()
+        {
+
+        }
+        #endregion
+
+        #region Members Property
+
+        // init memungkinkan inisialisasi hanya pada saat pembuatan objek. dan tidak dapat di gunakan lagi
+        public string? Model { get; init; }
+        public string? Make { get; init; }
+
+        /// <summary>
+        /// Kelas turunan yang mewarisi dari kelas ini diharapkan memberikan implementasi konkret 
+        /// (nilai aktual) untuk properti WheelCount.
+        /// </summary>
+        /// <value></value>
+        public abstract int WhellCount { get; }
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Dengan kata lain, kelas yang mewarisi dari kelas ini (kelas turunan) 
+        /// wajib memberikan implementasi konkret (kode aktual) untuk metode SoundHorn
+        /// </summary>
+        public abstract void SoundHorn();
+
+        public override string ToString()
+        {
+            return $"{GetType()} : {Make} {Model}";
+        }
+
+        #endregion
+    }
+
+
+    public class Car : Vehicle
+    {
+        #region Constructor
+        public Car()
+        {
+
+        }
+        #endregion
+
+        #region Members Property
+        // mengimplementasikan property abstract dari base class
+        public override int WhellCount => 4;
+        #endregion
+
+        #region Methods
+        // mengimplementasikan method dari base class
+        public override void SoundHorn()
+        {
+            Console.WriteLine($"Beep Beep");
+        }
+        #endregion
+    }
+
+
+    public class Motorcycle : Vehicle
+    {
+        #region Constructor
+        public Motorcycle()
+        {
+
+        }
+        #endregion
+
+        #region Members Property
+        // mengimplementasikan property abstract dari base class
+        public override int WhellCount => 2;
+        #endregion
+
+        #region Methods
+        // mengimplementasikan method dari base class
+        public override void SoundHorn()
+        {
+            Console.WriteLine($"Honk Honk");
+        }
+        #endregion
+    }
+}
