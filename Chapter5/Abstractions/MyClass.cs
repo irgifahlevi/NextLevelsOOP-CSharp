a0428296d4e553a37b3de7557106c8984fdaf828
irgifahlevi <irgifahlevi5@gmail.com>
2024-01-21 13:52:28 +0700

    abstractions and instance polymorphism

---
+++ Chapter5/Abstractions/MyClass.cs
@@ -0,0 +1,114 @@
+namespace Abstractions
+{
+    public interface IPerson
+    {
+        /// <summary>
+        /// Interface merupakan blue print yang menyediakan method dan properti tanpa memberikan 
+        /// implementasi konkret, yang mengimplementasikan hanyalah kelas kelas yang mengimplementasikan
+        /// interface
+        /// </summary>
+        /// <value></value>
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    /// <summary>
+    /// kelas abstrac memungkinka kelas tidak dapat di instance secara langsung, kelas abstrac
+    /// merupakan kelas dasar atau blue print
+    /// </summary>
+    public abstract class Employee : IPerson
+    {
+        #region Property
+        // property implement dari interface Iperson
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+
+        // property dari kelas Employee
+        public DateOnly StartDate { get; set; }
+
+        // virtual property kelas turunan mendapatkan opsi untuk mengimplementasikan 
+        // property DateTime
+        public virtual DateTime EndDate { get; set; }
+
+        //abstract property kelas turunan maka kelas turunannya (implementasi konkret) wajib memberikan
+        // implementasi untuk property EmployeeId tersebut
+        public abstract int EmployeeId { get; }
+        #endregion
+
+        // dari kelas turunan wajib mengimplementasikan method ProcessPayroll
+        public abstract bool ProcessPayroll();
+
+        // kelas turunan memiliki opsi untuk mengimplementasikan method Terminate
+        public virtual void Terminate(DateTime terminationEffectiveDate)
+        {
+            Console.WriteLine("Employee terminated");
+            EndDate = terminationEffectiveDate;
+        }
+
+        //derived can call or hide
+        public bool IsActive()
+        {
+            Console.WriteLine("Employee Active");
+            DateOnly current = DateOnly.FromDateTime(DateTime.Now);
+            return current > StartDate && DateTime.Now < EndDate;
+        }
+    }
+
+    public class ShiftWorker : Employee
+    {
+        #region Property
+        public TimeOnly ShiftStartTime { get; set; }
+        public override int EmployeeId { get => 1; }
+        #endregion
+
+        #region Method
+
+        // Method ProcessParyroll merupakan imploementasi konkret dari base class
+        public override bool ProcessPayroll()
+        {
+            Console.WriteLine("Shiftworker payroll");
+            return true;
+        }
+
+        // membuat method baru dari base class dengan menggunakan new
+        public new bool IsActive()
+        {
+            Console.WriteLine("Shiftworker active");
+            return false;
+        }
+        #endregion
+    }
+
+
+    public class Manager : Employee, IPerson
+    {
+
+        #region Property
+        // property dari kelas manager
+        public int NumberOfDirectReports { get; set; }
+
+        // overide property dari kelas dasar yang harus di implementasikan secara konkret
+        public override int EmployeeId { get => new System.Random().Next(1, 100); }
+        #endregion
+
+        #region Method
+        // Method abstract harus di implementasikan secara konkret, dari kelas dasar
+        public override bool ProcessPayroll()
+        {
+            Console.WriteLine("Manager payroll");
+            return true;
+        }
+
+        // implementasi method virtual dengan memberikan opsi kepada kelas trurunan
+        public override void Terminate(DateTime terminationEffectiveDate)
+        {
+            //perform manager specific termination steps
+            Console.WriteLine("Manager terminated");
+
+            // opsional memanggil method terminate dari base class 
+            base.Terminate(terminationEffectiveDate);
+        }
+        #endregion
+
+    }
+}
---
+++ Chapter5/Abstractions/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Abstractions;
+
+/// <summary>
+/// Membuat instance dari kelas ShiftWorker dan menetapkannya ke variabel dengan tipe Employee.
+/// (polymorphism)
+/// </summary>
+/// <value></value>
+Employee sw = new ShiftWorker
+{
+    FirstName = "staff",
+    LastName = "one",
+    StartDate = new DateOnly(2014, 2, 17),
+    ShiftStartTime = new TimeOnly(8, 30)
+};
+
+
+Console.WriteLine("=============Shift Worker=============");
+
+// mengakses method di class ShiftWorker abstract dari kelas dasar employee
+bool payrollProcessed = sw.ProcessPayroll();
+
+sw.Terminate(DateTime.Now);
+bool active = sw.IsActive();
+Console.WriteLine();
+
+
+Employee mgr = new Manager
+{
+    FirstName = "manager",
+    LastName = "one",
+    NumberOfDirectReports = 7
+};
+
+Console.WriteLine("=============Manager=============");
+
+// mengakses method di class manager abstract dari kelas dasar employee
+bool mgrPayrollProcessed = mgr.ProcessPayroll();
+
+// memanggil method dari kelas manager
+mgr.Terminate(DateTime.Now);
+
+// memanggil method di class employee
+bool mgrActive = mgr.IsActive();
+
