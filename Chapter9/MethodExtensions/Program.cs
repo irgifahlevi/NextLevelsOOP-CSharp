a9ea8eb28af765e497937f091df762698e578c7e
irgifahlevi <irgifahlevi5@gmail.com>
2024-02-05 20:19:55 +0700

    Method extensions

--- Chapter9/MethodExtensions/Program.cs
+++ Chapter9/MethodExtensions/Program.cs
@@ -9,3 +9,26 @@ string name = "Jane Black";
 // Dengan adanya kata kunci this string, kita dapat memanggil Right pada objek myString, 
 // meskipun sebenarnya Right adalah metode ekstensi.
 System.Console.WriteLine(name.Right(5));
+
+System.Console.WriteLine();
+System.Console.WriteLine("======Extension methods on collections======");
+System.Console.WriteLine();
+
+var names = new List<string> { "Jane", "Lucas", "Andrew", "Erick", "Nick", "Tyson", "Clara" };
+
+// mencari length terkecil pada list menggunakan lambda expression
+var sorter = names.MinBy((n) => n.Length);
+System.Console.WriteLine(sorter);
+
+// mengurutkan name dari yang lenght terkecil menggunakan lambda expression
+var orderBy = names.OrderBy((n) => n.Length);
+foreach (var data in orderBy)
+{
+    System.Console.WriteLine(data);
+}
+
+// chalenge
+int[] numbers = { 4, 7, 1, 9, 2, 5 };
+double medianValue = numbers.Median();
+
+System.Console.WriteLine(medianValue);
--- Chapter9/MethodExtensions/StringExtensionsSample.cs
+++ Chapter9/MethodExtensions/StringExtensionsSample.cs
@@ -6,6 +6,7 @@ namespace MethodExtensions
         // Misalnya: "Hello, World!".Right(5) akan mengembalikan "World!".
         // Jika input string kosong atau null, metode ini mengembalikan string kosong.
         // Dengan menambahkan this string, kita memberi tahu C# bahwa metode Right adalah ekstensi khusus untuk tipe data string. 
+        // namun dengan catatan class harus d buat static
         public static string Right(this string input, int numChars)
         {
             // Memeriksa apakah input string null atau kosong.
@@ -23,6 +24,34 @@ namespace MethodExtensions
 
             // Menggunakan Substring untuk mengambil sejumlah karakter terakhir.
             return input.Substring(input.Length - numChars);
+        }
+
+
+        // Chalenge
+        public static double Median(this int[] numbers)
+        {
+            // menyalin array dengan casting operator
+            int[] cNumbers = (int[])numbers.Clone();
+
+            // mengurutkan array secara ascending
+            Array.Sort(numbers);
+
+            // hitung jumlah array
+            int size = numbers.Count();
+
+            // menghitung index tengah array
+            int mid = size / 2;
+
+            // menghitung nilai median
+
+            /// <summary>
+            /// Jika jumlah elemen array ganjil, maka nilai median adalah elemen di tengah array. 
+            /// Jika jumlah elemen array genap, maka nilai median adalah rata-rata dari dua elemen
+            /// di tengah array.
+            /// </summary>
+
+            double median = (size % 2 != 0) ? (double)cNumbers[mid] : ((double)cNumbers[mid] + (double)cNumbers[mid - 1]) / 2;
+            return median;
         }
     }
 }
